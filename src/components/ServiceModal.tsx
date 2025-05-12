4536e8f879f31ef5583bfe464f9b89966a1cfa5f
emarishkin <ee.marishkin@yandex.ru>
2025-05-12 13:38:59 +0400

    service-modal+

--- src/components/ServiceModal.tsx
+++ src/components/ServiceModal.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import '../styles/ModalService.css'
 
 interface ServiceModalProps{
@@ -7,15 +7,43 @@ interface ServiceModalProps{
 }
 
 export const ServiceModal:FC<ServiceModalProps> = ({onClose,isOpen}) => {
-
+    
+    const [name,setName] = useState<string>('')
+    const [number,setNumber] = useState<string>('')
+    const [quast,setQuast] = useState<string>('')
     
     return(
         <div>
             {isOpen && (
                 <div className="modal-service">
                     <div className="modal2" onClick={(e) => e.stopPropagation()}>
-                        
-                        <button onClick={onClose}>close</button>
+                        <form className="popup__content">
+                            <h2 className="popup__content-title">Получить бесплатную консультацию менеджера по логистике</h2> 
+                            <p className="popup__content-text">Оставьте, пожалуйста, свои контактные данные, и наш опытный менеджер свяжется с вами в кратчайшие сроки для обсуждения деталей.</p>
+                            <div className="modal2-inputs">
+                                <input
+                                 type="text"
+                                 value={name}
+                                 onChange={(e:ChangeEvent<HTMLInputElement>)=>setName(e.target.value)}
+                                 placeholder="Введите имя" 
+                                 />
+
+                                <input
+                                 type="text"
+                                 value={number}
+                                 onChange={(e:ChangeEvent<HTMLInputElement>)=>setNumber(e.target.value)}
+                                 placeholder="введите номер телефона" 
+                                 />
+
+                                <textarea
+                                 value={quast}
+                                 onChange={(e)=>setQuast(e.target.value)}
+                                 placeholder="ВВедите имя" 
+                                 />  
+                            </div>
+
+                            <button className="popup__content-close" onClick={onClose}>✖</button>
+                        </form>
                     </div>
                 </div>
             )}
