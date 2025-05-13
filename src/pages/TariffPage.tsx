be067e7e88a30e0187ff5c1bf7ac614810c19412
emarishkin <ee.marishkin@yandex.ru>
2025-05-13 18:32:22 +0400

    route-Tariff

--- src/pages/TariffPage.tsx
+++ src/pages/TariffPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { TariffsSection } from "../components/TariffsSection";
+import { InfoTwo } from "../components/InfoTwo";
 
 
 export const TariffPage: React.FC = () => {
   return (
     <div>
       <TariffsSection />
+      <InfoTwo />
     </div>
   );
 };
